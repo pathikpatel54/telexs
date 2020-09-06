1b485a366b5f7c05f0a374cc6061a4a841e36bcf
Pathik Patel <pathikpatel54@gmail.com>
2020-09-06 17:12:59 +0530

    Update

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,11 @@
+import axios from "axios"
+import { FETCH_USER_ERROR, FETCH_USER_SUCCESS } from "./types"
+
 export const fetchUser = () => async dispatch => {
-    
+    try {
+        response = await axios.get("/api/user")
+            dispatch({ type: FETCH_USER_SUCCESS})
+    } catch (e) {
+        return dispatch({ type: FETCH_USER_ERROR, payload: e })
+    }
 }
