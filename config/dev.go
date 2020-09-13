91f5021e0f61d957363aca9dd8eceafc19ace221
pathikpatel54 <36487705+pathikpatel54@users.noreply.github.com>
2020-09-13 14:10:03 +0530

    Delete dev.go

--- config/dev.go
+++
@@ -1,7 +0,0 @@
-package config
-
-var devKeys = vars{
-	GoogleClientID:     "114396290296-us0oa4gt47jakig0bl9jp6ecq6qqnhsk.apps.googleusercontent.com",
-	GoogleClientSecret: "VwLsQmOs-dySs7JZVNJqHO2C",
-	MongodbUser:        "V1qAUg8sSzICUG9q",
-}
